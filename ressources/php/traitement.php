28c845ad4c9bed4510aafe164f93c17a69edf5bb
WILMES <maxencewilmes@gmail.com>
2019-06-04 16:31:08 +0200

    Update Formulaire Contact

--- ressources/php/traitement.php
+++ ressources/php/traitement.php
@@ -1,6 +1,25 @@
 <?php 
+$conn = new mysqli("localhost", "phpmyadmin", "admin", "table_test");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 $nom = $_POST['name'];
 $mail = $_POST['mail'];
 $message = $_POST['message'];
 
+$sql_items = "INSERT INTO Contact (NAME, mail, message) VALUES ('$nom', '$mail', '$message')";
+$check_name = "SELECT id FROM Contact WHERE NAME = $nom AND Mail=$mail AND Message=$message";
+if(empty($check_name)){
+	if ($conn -> query($sql_items) === TRUE) {
+		echo "Votre demande a bien été enregistrée.";
+	} else {
+		echo "ERROR : ". $sql_items . "<br>" . $conn ->error;
+	}
+}else{
+	echo "Votre message a déjà été envoyé.";
+}
+
+
+$conn -> close();
